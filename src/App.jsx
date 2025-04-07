de8b9d6155d675bf9e5a21eb93681bbfe4faf6c7
Nuhamin Tibebu <nuhamintibebu965@gmail.com>
2025-04-08 00:46:46 +0300

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className="search-section">
+        <form action="#" className="search-form">
+          <input type="search" placeholder='Enter a city name' className="search-input" required />
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
